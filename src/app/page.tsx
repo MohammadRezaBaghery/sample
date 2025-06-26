50b62430b2ba9c5c06ca289cf45e9958551dc806
Amir Zare <ahzarenejad2127@gmail.com>
2025-06-26 23:29:30 +0330

    feat: test some thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to tabibaan.ir →
+          Go to tabibaan.irr →
         </a>
       </footer>
     </div>
